1a03338c4d61b1eab4018efdb03e14ba29a4a0f6
Shaoyu Yang <100203773+shaoyuyoung@users.noreply.github.com>
2024-02-05 19:43:13 +0800

    Update data_filter.py

--- data_prepare/data_filter.py
+++ data_prepare/data_filter.py
@@ -1,83 +1,86 @@
-"""
-Used to clean data from dumped logs
-"""
 from lxml import etree
 import pandas as pd
 from bs4 import BeautifulSoup
-from itertools import islice
-import numpy as np
 from tqdm import *
 import re
 
-lans = ['python', 'java', 'javascript', 'c#', 'html', 'php']
-for lan in lans:
-    to = pd.DataFrame(columns=['Id', 'Body', 'Code', 'Title', 'Score', 'Tags',
-                               'accepted'])
-    print("start::::", lan)
-    context = etree.iterparse('Posts.xml', events=('end',), tag='row')
-    lanTag = "<" + lan + ">"
-    bar = context
-    CodeList = []
-    BodyList = []
-    TitleList = []
-    ScoreList = []
-    IdList = []
-    Tags = []
-    Body, Title, to = None, None, None
-    for event, element in tqdm(bar):
-        temp = element.attrib
-        body, score, title, tag, accepted, postTypeId, Id, discription, code = '', 0, '', '', 0, 0, 0, '', ''
+LANGUAGES = ['python', 'java', 'c#', 'html', 'go', 'ruby', 'javascript', 'php']
+XML_FILE_PATH = 'Posts.xml'
 
-        for key in temp:
-            if key == "PostTypeId":
-                postTypeId = temp[key]
-                if postTypeId != '1':
-                    break
-            elif key == "Id":
-                Id = temp[key]
-            elif key == "Body":
-                body = temp[key]
-            elif key == "Score":
-                score = temp[key]
-                score = int(score)
-            elif key == "Title":
-                title = temp[key]
-            elif key == "Tags":
-                tag = temp[key]
-            elif key == "AcceptedAnswerId":
-                accepted = temp[key]
-                accepted = int(accepted)
-        if (score >= 10) and postTypeId == '1' and (lanTag in tag) and accepted != 0 and (
-                "</code></pre>" in body):
-            body = re.sub('[^a-zA-Z0-9~!@#$%^&*()_+`\-={}\[\]|\\\:";\'<>?,./ ]', "", body)
-            title = re.sub('[^a-zA-Z0-9~!@#$%^&*()_+`\-={}\[\]|\\\:";\'<>?,./ ]', "", title)
-            pattern = re.compile(r'(.*?)<pre(.*?)</pre>', re.S)
-            discription = ''
-            code = ''
-            while (len(body)):
-                match = re.match(pattern, body)
-                if match:
-                    discription = discription + ' ' + match.group(1)
-                    code = code + ' ' + '<pre' + match.group(2) + '</pre>'
-                    body = body[match.end():]
-                else:
-                    discription = discription + ' ' + body
-                    body = ""
+if __name__ == "__main__":
+    for lan in LANGUAGES:
+        save_file = f"{lan}.csv"
+        to = pd.DataFrame(columns=['Id', 'Body', 'Code', 'Title', 'Score', 'Tags',
+                                   'accepted'])
+        to.to_csv(save_file, header=True, index=False)
+        print(f"{lan} starts!")
+        context = etree.iterparse(XML_FILE_PATH, events=('end',), tag='row')
+        lanTag = "<" + lan + ">"
+        bar = tqdm(context)
+        CodeList = []
+        BodyList = []
+        TitleList = []
+        ScoreList = []
+        IdList = []
+        Tags = []
+        Body, Title, to = None, None, None
+        for event, element in bar:
+            temp = element.attrib
+            body, score, title, tag, accepted, postTypeId, Id, description, code = '', 0, '', '', 0, 0, 0, '', ''
 
-            discription = BeautifulSoup(discription, 'html.parser').get_text()
-            code = BeautifulSoup(code, 'html.parser').get_text()
+            for key in temp:
+                if key == "PostTypeId":
+                    postTypeId = temp[key]
+                    if postTypeId != '1':
+                        break
+                elif key == "Id":
+                    Id = temp[key]
+                elif key == "Body":
+                    body = temp[key]
+                elif key == "Score":
+                    score = temp[key]
+                    score = int(score)
+                elif key == "Title":
+                    title = temp[key]
+                elif key == "Tags":
+                    tag = temp[key]
+                elif key == "AcceptedAnswerId":
+                    accepted = temp[key]
+                    accepted = int(accepted)
+            if score >= 10 and postTypeId == '1' and lanTag in tag and accepted != 0 and "</code></pre>" in body:
+                # remove Mojibake
+                body = re.sub('[^a-zA-Z0-9~!@#$%^&*()_+`\-={}\[\]|\\\:";\'<>?,./ ]', "", body)
+                title = re.sub('[^a-zA-Z0-9~!@#$%^&*()_+`\-={}\[\]|\\\:";\'<>?,./ ]', "", title)
+                # extract description and code
+                pattern = re.compile(r'(.*?)<pre(.*?)</pre>', re.S)
+                description = ''
+                code = ''
+                while len(body):
+                    match = re.match(pattern, body)
+                    if match:
+                        description = description + ' ' + match.group(1)
+                        code = code + ' ' + '<pre' + match.group(2) + '</pre>'
+                        body = body[match.end():]
+                    else:
+                        description = description + ' ' + body
+                        body = ""
+                # remove markets
+                description = BeautifulSoup(description, 'html.parser').get_text()
+                code = BeautifulSoup(code, 'html.parser').get_text()
 
+                # clap
+                description = description.replace('\n', " ")
+                description = re.sub(" +", " ", description)  # merge space
+                code = code.replace('\n', ' ')
+                code = re.sub(" +", " ", code)  # merge space
 
-            discription = discription.replace('\n', " ")
-            discription = re.sub(" +", " ", discription)[1:]
-            code = code.replace('\n', ' ')
-            code = re.sub(" +", " ", code)[1:]
+                # Encode and decode, convert to utf-8
+                description = description.encode('utf-8', 'ignore').decode().strip()
+                code = code.encode('utf-8', 'ignore').decode().strip()
+                title = title.encode('utf-8', 'ignore').decode()
+                tag = tag.encode('utf-8', 'ignore').decode()
 
-            discription = discription.encode('utf-8', 'ignore').decode()
-            code = code.encode('utf-8', 'ignore').decode()
-            title = title.encode('utf-8', 'ignore').decode()
-            tag = tag.encode('utf-8', 'ignore').decode()
-
-            to = pd.DataFrame([[Id, discription, code, title, score, tag, accepted]],
-                              columns=['Id', 'Body', 'Code', 'Title', 'Score', 'Tags', 'accepted'])
-        element.clear()
+                to = pd.DataFrame([[Id, description, code, title, score, tag]],
+                                  columns=['Id', 'Desc', 'Code', 'Title', 'Score', 'Tags'])
+                to.to_csv(save_file, header=False, index=False, mode='a+')
+            element.clear()
